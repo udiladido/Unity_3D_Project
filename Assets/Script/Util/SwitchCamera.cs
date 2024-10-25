f7ca31999670c80cc78b3878413eab2b62e31b39
udiladido <89720389+udiladido@users.noreply.github.com>
2024-10-25 21:12:20 +0900

    feat : 카메라 페이드 인 아웃 기능 추가
    https://youtu.be/NYdLInZVsAM?si=BiBElnOYytzPN_Aa

--- Assets/Script/Util/SwitchCamera.cs
+++ Assets/Script/Util/SwitchCamera.cs
@@ -1,15 +1,19 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SwitchCamera : MonoBehaviour
 {
 
     public GameObject TirdPersonCamera;
     public GameObject FirstPersonCamera;
-
+    public Image pannel;
 
     private PlayerController controller;
+
+
+    float time = 0f;
+    public float F_time = 1f;
 
 
     private void Start()
@@ -21,6 +25,8 @@ public class SwitchCamera : MonoBehaviour
 
     void ViewPointSwitch()
     {
+
+        Fade();
 
         if (TirdPersonCamera.activeInHierarchy)
         {
@@ -38,7 +44,47 @@ public class SwitchCamera : MonoBehaviour
 
 
     }
-    
 
+    public void Fade()
+    {
+
+        StartCoroutine(FadeScreen());
+    
+    }
+    IEnumerator FadeScreen()
+    {
+
+        pannel.gameObject.SetActive(true);
+        time = 0f;
+        Color alpha = pannel.color;
+        while (alpha.a < 1f)
+        { 
+        
+            time += Time.deltaTime / F_time;
+            alpha.a = Mathf.Lerp(0, 1, time);
+            pannel.color = alpha;
+            yield return null;
+        }
+
+        time = 0f;
+
+        yield return  new WaitForSeconds(F_time);
+
+        while (alpha.a > 0f)
+        {
+
+            time += Time.deltaTime / F_time;
+            alpha.a = Mathf.Lerp(1, 0, time);
+            pannel.color = alpha;
+            yield return null;
+        }
+
+        pannel.gameObject.SetActive(false);
+        yield return null;
+    
+    }
+
+
+    
 
 }
