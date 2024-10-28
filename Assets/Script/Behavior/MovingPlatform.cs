cb45fddd0464ea61623a19f1e35646d9365568aa
udiladido <89720389+udiladido@users.noreply.github.com>
2024-10-28 15:13:57 +0900

    feat : 움직이는 플랫폼 구현
    https://youtu.be/rO19dA2jksk?si=xxLrEelNId0fJTr8

---
+++ Assets/Script/Behavior/MovingPlatform.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+
+
+    public GameObject Player;
+
+
+    private void Start()
+    {
+        Player = CharacterManager.Instance.Player.gameObject; 
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject == Player)
+        {
+
+            Player.transform.parent = transform;
+        
+        }
+
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (other.gameObject == Player)
+        {
+
+            Player.transform.parent = null;
+
+        }
+
+    }
+
+
+}
