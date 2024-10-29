ec87cda779df30a62da915abb72957f00e910ad6
udiladido <89720389+udiladido@users.noreply.github.com>
2024-10-29 22:14:42 +0900

    feat : 장비 장착 구현
    전략 패턴 활용 시도

---
+++ Assets/Script/Util/HeadSkin.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadSkin : ChangeSkinnedMesh
+{
+    public override void ChangeMesh(Mesh newMesh)
+    {
+        base.ChangeMesh(newMesh);
+
+    }
+
+  
+}
